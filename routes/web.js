9ff5e70bb509ad5774b83e2f1ad87bca343e684b
zackenstien <trimorphyt@gmail.com>
2020-08-03 16:05:09 -0400

    :white_check_mark: Test with sessions.

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,14 @@
+"use strict"
+
 const Route = require('../app/route');
 
 Route.redirect('/search', 'https://google.com');
 
-Route.get('/test', () => {
-    return `Hello, world!`;
+Route.get('/test', function() {
+    if (!this.session.get('firstLoad')) {
+        this.session.set('firstLoad', true);
+        return `Hello, world!`;
+    } else {
+        return `Hello again, world!`;
+    }
 });
