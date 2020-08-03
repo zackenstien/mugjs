9c93579aa179417a2683d342f31abe7afa9111e6
zackenstien <trimorphyt@gmail.com>
2020-08-03 19:29:26 -0400

    :art: Remove unused code.

--- routes/web.js
+++ routes/web.js
@@ -1,11 +1,7 @@
-"use strict"
-
 const Route = require('../app/route'),
       View = require('../template/view');
 
-Route.redirect('/search', 'https://google.com');
-
-Route.get('/test', function() {
+Route.get('/', function() {
     if (!this.session.get('firstLoad')) {
         this.session.set('firstLoad', true);
         return View('index', {
