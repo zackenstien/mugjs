b3df6b8e52386270881315ddd5d9412e01a17105
zackenstien <trimorphyt@gmail.com>
2020-08-03 14:13:13 -0400

    :white_check_mark: Downgrade to super simple example.

--- routes/example.js
+++ routes/example.js
@@ -1,19 +1,8 @@
 const Route = require('../app/route');
-const Server = require('../app/server')
+const Server = require('../app/server');
 
 Route.get('*', () => {
-    return `404 Not Found.  If you believe that this is an error, please contact the website admin.`;
+    return `Hello world!`;
 });
-
-Route.get('/hello/:name', (name) => {
-    if (name.toLowerCase() == "world") {
-        return `Please go to '/helloworld'.`
-    }
-    return `Hello, ${name}!`;
-});
-
-Route.get('/helloworld', () => {
-    return `Hello, World!`
-})
 
 Server.start(80);
